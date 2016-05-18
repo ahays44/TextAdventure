ceacc5fca58d94ec16e22db415016da4e5a77402
austinhays <ahays.target@yahoo.com>
2016-05-18 12:03:23 -0400

    TextAdventure - String to int

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -16,5 +16,15 @@ public class Main {
         player.chooseWeapon();
         player.chooseLocation();
 
+        System.out.println("Type a number.");
+        String num = scanner.nextLine();
+        int numInt = Integer.valueOf(num);
+
+        if (numInt < 0) {
+            System.out.println("That's a negative number!");
+        }
+        else {
+            System.out.println("That's a postive number!");
+        }
     }
 }
